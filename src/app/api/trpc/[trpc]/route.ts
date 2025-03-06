a8f2f5e5664e423fba691d27b83d77a38596314e
hienleke <lekehien5431@gmail.com>
2025-03-06 18:46:50 +0700

    set runtime to nodejs for TRPC route handler

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -2,6 +2,8 @@ import { createTRPCContext } from '../../../../server/trpc';
 import { appRouter } from '../../../../server/trpc';
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 
+export const runtime = "nodejs";
+
 const handler = (req: Request) =>
   fetchRequestHandler({
     endpoint: '/api/trpc',
